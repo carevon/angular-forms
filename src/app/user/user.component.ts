dbeb4326c8c90d129edf534d5067912fc7c480fa
Logon Aluno <logonaluno@local.com>
2019-08-20 20:29:46 -0300

    First commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-user', // é a tag pra adicionar um webcomponent
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+  formUser:FormGroup; //declarar uma variavel do tipo FormGroup
+  constructor(private formBuilder:FormBuilder) { 
+    //Estrutura do formulario de usuário com dados pré definidos
+
+    this.formUser = this.formBuilder.group({
+      nome: [{value:"João", disabled:false}],
+      sobrenome: [{value:"João", disabled:false}],
+      email: [{value:"joão@gmail.com", disabled:false}],
+      telefone: [{value:"+55119656565", disabled:false}]
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
